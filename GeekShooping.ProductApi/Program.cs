f01ace03ba0d7b4a8a9a14390a7b7b5e4720c111
angeloqg <angeloqg@hotmail.com>
2022-09-07 17:52:20 -0300

    Definindo a conexão com o banco

--- GeekShooping.ProductApi/Program.cs
+++ GeekShooping.ProductApi/Program.cs
@@ -1,13 +1,23 @@
+using GeekShooping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connection = builder.Configuration["MySqlConnection:MySqlConnetionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, 
+                                                       ServerVersion.AutoDetect(connection)));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+var config = app.Configuration;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
