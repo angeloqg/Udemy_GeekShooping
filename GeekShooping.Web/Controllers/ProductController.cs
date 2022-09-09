698fb40d1a406b322cc267c8b3d815478d96ced6
angeloqg <angeloqg@hotmail.com>
2022-09-09 19:16:01 -0300

    Criação da tela de Update do produto e ajuste na Controller

--- GeekShooping.Web/Controllers/ProductController.cs
+++ GeekShooping.Web/Controllers/ProductController.cs
@@ -37,5 +37,27 @@ namespace GeekShooping.Web.Controllers
 
             return View(model);
         }
+
+        public async Task<IActionResult> ProductUpdate(long id)
+        {
+            var model = await _productService.FindByIdProduct(id);
+            if (model != null) return View(model);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+
+                if (response != null) return RedirectToAction(nameof(ProductIndex));
+
+            }
+
+            return View(model);
+        }
     }
 }
