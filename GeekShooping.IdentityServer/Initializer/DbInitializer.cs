fee46b989fba12cdf72dda8092de268bd562a6cd
angeloqg <angeloqg@hotmail.com>
2022-09-27 22:24:22 -0300

    Até aqui, funcionando, kkkk

---
+++ GeekShooping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,76 @@
+﻿using GeekShooping.IdentityServer.Configuration;
+using GeekShooping.IdentityServer.Model;
+using GeekShooping.IdentityServer.Model.Context;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GeekShooping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MySqlContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<IdentityRole> _role;
+
+        public DbInitializer(MySqlContext context,
+            UserManager<ApplicationUser> user,
+            RoleManager<IdentityRole> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initialize()
+        {
+            if (_role.FindByNameAsync(IdentityConfiguration.Admin).Result != null) return;
+            _role.CreateAsync(new IdentityRole(
+                IdentityConfiguration.Admin)).GetAwaiter().GetResult();
+            _role.CreateAsync(new IdentityRole(
+                IdentityConfiguration.Client)).GetAwaiter().GetResult();
+
+            ApplicationUser admin = new ApplicationUser()
+            {
+                UserName = "angelo-admin",
+                Email = "angelo-admin@email.com.br",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (34) 12345-6789",
+                FirstName = "Angelo",
+                LastName = "Admin"
+            };
+
+            _user.CreateAsync(admin, "Secreto123$").GetAwaiter().GetResult();
+            _user.AddToRoleAsync(admin,
+                IdentityConfiguration.Admin).GetAwaiter().GetResult();
+            var adminClaims = _user.AddClaimsAsync(admin, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{admin.FirstName} {admin.LastName}"),
+                new Claim(JwtClaimTypes.GivenName, admin.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, admin.LastName),
+                new Claim(JwtClaimTypes.Role, IdentityConfiguration.Admin)
+            }).Result;
+
+            ApplicationUser client = new ApplicationUser()
+            {
+                UserName = "angelo-client",
+                Email = "angelo-client@email.com.br",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (34) 12345-6789",
+                FirstName = "Angelo",
+                LastName = "Client"
+            };
+
+            _user.CreateAsync(client, "Secreto123$").GetAwaiter().GetResult();
+            _user.AddToRoleAsync(client,
+                IdentityConfiguration.Client).GetAwaiter().GetResult();
+            var clientClaims = _user.AddClaimsAsync(client, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{client.FirstName} {client.LastName}"),
+                new Claim(JwtClaimTypes.GivenName, client.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, client.LastName),
+                new Claim(JwtClaimTypes.Role, IdentityConfiguration.Client)
+            }).Result;
+        }
+    }
+}
--- GeekShooping.ProductApi/Program.cs
+++ GeekShooping.ProductApi/Program.cs
@@ -38,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
