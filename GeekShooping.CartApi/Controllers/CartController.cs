f37b7de8d8689c2c3a6bfddd448fe3c20ff4e0a2
angeloqg <angeloqg@hotmail.com>
2022-10-07 11:28:07 -0300

    API - Criando a controller CartController(GeekShooping.CartApi)

---
+++ GeekShooping.CartApi/Controllers/CartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.CartApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+    }
+}
