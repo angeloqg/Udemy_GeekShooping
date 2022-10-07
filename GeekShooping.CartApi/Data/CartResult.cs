4151df9fd701d0db1d99ba560946c623f2f233b1
angeloqg <angeloqg@hotmail.com>
2022-10-07 15:37:17 -0300

    API - Implementação do CartResult (GeekShooping.CartApi)

---
+++ GeekShooping.CartApi/Data/CartResult.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShooping.CartApi.Data
+{
+    public class CartResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+    }
+}
