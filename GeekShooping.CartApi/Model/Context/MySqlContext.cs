de04534fae13d6489f282608cdfd668da019d97e
angeloqg <angeloqg@hotmail.com>
2022-10-07 12:23:52 -0300

    API - Ajustando o DbContext (GeekShooping.CartApi)

--- GeekShooping.CartApi/Model/Context/MySqlContext.cs
+++ GeekShooping.CartApi/Model/Context/MySqlContext.cs
@@ -4,8 +4,9 @@ namespace GeekShooping.CartApi.Model.Context
 {
     public class MySqlContext : DbContext
     {
-        public MySqlContext() { }
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
     }
 }
