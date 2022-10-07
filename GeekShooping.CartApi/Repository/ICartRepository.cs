d8e4ee86b7df3dd67682580737117d2b0dd7c708
angeloqg <angeloqg@hotmail.com>
2022-10-07 15:26:32 -0300

    API - Implementando o repositório (GeekShooping.CartApi)

---
+++ GeekShooping.CartApi/Repository/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShooping.CartApi.Data.ValueObjects;
+
+namespace GeekShooping.CartApi.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartVO> FindCartUserId(string userId);
+        Task<CartVO> SaveOrUpdateCart(CartVO cart);
+        Task<bool> RemoveFromCart(long cartDetailsId);
+        Task<bool> ApplyCoupon(string userId, string couponCode);
+        Task<bool> RemoveCoupon(string userId);
+        Task<bool> ClearCart(string userId);
+    }
+}
