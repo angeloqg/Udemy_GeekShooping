92080c6aeb16cd9271d548eaa08ab98ae17f391d
angeloqg <angeloqg@hotmail.com>
2022-10-08 12:41:50 -0300

    WEB - Criando o CartController (GeekShooping.Web)

---
+++ GeekShooping.Web/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using GeekShooping.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
