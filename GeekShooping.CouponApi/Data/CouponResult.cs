2dd88d98b523eb9e925a1afe8c4cb768ab3f8988
angeloqg <angeloqg@hotmail.com>
2022-10-09 00:28:38 -0300

    API - Criando o CouponResult (GeekShooping.CouponApi)

---
+++ GeekShooping.CouponApi/Data/CouponResult.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShooping.CouponApi.Data
+{
+    public class CouponResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+    }
+}
