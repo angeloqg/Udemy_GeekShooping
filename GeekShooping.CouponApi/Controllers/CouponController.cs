4eac4f7561c4761d5c854b7e3339126b9b6b00c4
angeloqg <angeloqg@hotmail.com>
2022-10-09 01:02:19 -0300

    API - Ajustando a controller (GeekShooping.CouponApi)

--- GeekShooping.CouponApi/Controllers/CouponController.cs
+++ GeekShooping.CouponApi/Controllers/CouponController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GeekShooping.CouponApi.Data;
+using GeekShooping.CouponApi.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShooping.CouponApi.Controllers
@@ -7,5 +9,43 @@ namespace GeekShooping.CouponApi.Controllers
     [ApiController]
     public class CouponController : ControllerBase
     {
+        private readonly ICouponRepository _repository;
+        private CouponResult _result { get; set; }
+
+        public CouponController(ICouponRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+
+            _result = new CouponResult();
+        }
+
+        [HttpGet("find-coupon{couponcode}")]
+        public async Task<IActionResult> FindCoupon(string? couponcode)
+        {
+            var coupom = await _repository.GetCouponByCouponCode(couponcode);
+
+            if (coupom != null)
+            {
+                _result = new CouponResult
+                {
+                    Success = true,
+                    Message = "Coupom encontrado com sucesso",
+                    Data = coupom
+                };
+
+                return Ok(_result);
+            }
+            else
+            {
+                _result = new CouponResult
+                {
+                    Success = false,
+                    Message = "Nenhum coupon encontrado",
+                    Data = null
+                };
+
+                return Ok(_result);
+            }
+        }
     }
 }
