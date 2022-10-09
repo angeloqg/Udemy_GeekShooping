5f059582aa418a14765a9fd6e0f7f507ff5b33f0
angeloqg <angeloqg@hotmail.com>
2022-10-08 23:54:56 -0300

    API - Criando a controller CouponController (GeekShooping.CouponApi)

---
+++ GeekShooping.CouponApi/Controllers/CouponController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.CouponApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+    }
+}
