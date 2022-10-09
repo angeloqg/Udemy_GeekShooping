0b27f38715554e9b6e9f4711ccddc6d2a5a49479
angeloqg <angeloqg@hotmail.com>
2022-10-09 00:22:58 -0300

    API  - Ajustando as entidades (GeekShooping.CouponApi)

--- GeekShooping.CouponApi/Model/Context/MySqlContext.cs
+++ GeekShooping.CouponApi/Model/Context/MySqlContext.cs
@@ -5,6 +5,6 @@ namespace GeekShooping.CouponApi.Model.Context
     public class MySqlContext : DbContext
     {
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
     }
 }
