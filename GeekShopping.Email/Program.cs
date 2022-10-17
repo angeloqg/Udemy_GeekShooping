368e79dbf0fa66b7d57b715656b8046aac6f9cb4
angeloqg <angeloqg@hotmail.com>
2022-10-17 19:37:12 -0300

    EMAIL - Implementando o DbContext, Model, PaymentConsumer e Migrations (GeekShopping.Email)

--- GeekShopping.Email/Program.cs
+++ GeekShopping.Email/Program.cs
@@ -1,6 +1,26 @@
+using GeekShopping.Email.MessageConsumer;
+using GeekShopping.Email.Model.Context;
+using GeekShopping.Email.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Configure Connection Database
+var connection = builder.Configuration["MySqlConnection:MySqlConnetionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection,
+                                                       ServerVersion.AutoDetect(connection)));
+
+// Dependency Injection
+var builderInjection = new DbContextOptionsBuilder<MySqlContext>();
+builderInjection.UseMySql(connection, ServerVersion.AutoDetect(connection));
+
+builder.Services.AddSingleton(new EmailRepository(builderInjection.Options));
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
